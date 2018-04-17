0ea1186c8ab7ae0d2189e238180465d6098cb488
unknown <magg.slb@gmail.com>
2018-04-17 20:18:12 +0100

    tabNav added

--- client/mobileApp/src/components/MainScreen.js
+++ client/mobileApp/src/components/MainScreen.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Container, Content, List, ListItem, Header, Button, Icon /* All native-base components used in this file */ } from 'native-base';
+import TabNav from './TabNav';
+import { Icon } from 'native-base';
 
 export default class MainScreen extends Component {
-
     static navigationOptions = {
         title: "Meet Mindera",
-        //headerLeft: <Icon name={menu}/>,
-        headerRight: <Icon name={search}/>
+        headerLeft: <Icon name={search}/>,
+        //headerRight: <Icon name={search}/>
     };
 
     render(){
         return (
-            <View style = {styles.container}>
-                <Text>MainScreen</Text>
-            </View>
+            <TabNav/>
         )
     }
 }
--- client/mobileApp/src/components/TabNav.js
+++ client/mobileApp/src/components/TabNav.js
@@ -1,1 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { TabNavigator } from 'react-navigation';
+import EventsScreen from "./EventsScreen";
+import VacanciesScreen from "./VacanciesScreen";
+
+const TabNav = TabNavigator({
+    Events: {
+        screen: EventsScreen
+    },
+    Vacancies: {
+        screen: VacanciesScreen
+    }
+});
+
+export default TabNav;
